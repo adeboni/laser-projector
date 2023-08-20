8f25d78f2a0201b12be68df682b00c1e7da13729
Alex DeBoni <alexander.deboni@gmail.com>
2023-08-20 12:10:53 -0700

    Added equations and improved calibration mode

--- software/laser_controller/Calibration.cpp
+++ software/laser_controller/Calibration.cpp
@@ -7,21 +7,23 @@ int selectedLaser = 0;
 int selectedMode = 0;
 
 void offsetAlignment() {
-  lasers[selectedLaser].setColorRGB(0, 255, 0);
-  lasers[selectedLaser].on();
-
+  int laserIndex = selectedLaser;
+  
   float _xs, _ys;
-  lasers[selectedLaser].getScale(_xs, _ys);
+  lasers[laserIndex].getScale(_xs, _ys);
   gpio.setCVTarget(0, (int)(_xs * 100), 10);
   gpio.setCVTarget(1, (int)(_ys * 100), 10);
 
   int _xo, _yo;
-  lasers[selectedLaser].getOffset(_xo, _yo);
+  lasers[laserIndex].getOffset(_xo, _yo);
   gpio.setCVTarget(2, _xo, 200);
   gpio.setCVTarget(3, _yo, 200);
 
   int _x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4;
-  lasers[selectedLaser].getClipArea(_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4);
+  lasers[laserIndex].getClipArea(_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4);
+
+  lasers[laserIndex].setColorRGB(0, 255, 0);
+  lasers[laserIndex].on();
 
   while (selectedMode == MODE_OFFSET) {
     int xs = gpio.getCV(0, 5, 150);
@@ -29,8 +31,8 @@ void offsetAlignment() {
     int xo = gpio.getCV(2, -2000, 2000);
     int yo = gpio.getCV(3, -2000, 2000);
    
-    lasers[selectedLaser].setScale(xs / 100.0, ys / 100.0);
-    lasers[selectedLaser].setOffset(xo, yo);
+    lasers[laserIndex].setScale(xs / 100.0, ys / 100.0);
+    lasers[laserIndex].setOffset(xo, yo);
     
     /*
     lasers[selectedLaser].sendTo(2048, 0);
@@ -38,29 +40,33 @@ void offsetAlignment() {
     lasers[selectedLaser].sendTo(2048, 4095);
     lasers[selectedLaser].sendTo(0, 2048);
     */
-
-    lasers[selectedLaser].sendTo(_x1 + 10, _y1 + 10);
-    lasers[selectedLaser].sendTo(_x2 - 10, _y2 + 10);
-    lasers[selectedLaser].sendTo(_x3 - 10, _y3 - 10);
-    lasers[selectedLaser].sendTo(_x4 + 10, _y4 - 10);
-
+    
+    lasers[laserIndex].sendTo(_x1 + 10, _y1 + 10);
+    lasers[laserIndex].sendTo(_x2 - 10, _y2 + 10);
+    lasers[laserIndex].sendTo(_x3 - 10, _y3 - 10);
+    lasers[laserIndex].sendTo(_x4 + 10, _y4 - 10);
+   
     checkButtons();
+
+    if (selectedLaser != laserIndex)
+      break;
   }
+
+  lasers[laserIndex].off();
 }
 
 void clippingTopAlignment() {
-  lasers[selectedLaser].setColorRGB(255, 0, 255);
-  lasers[selectedLaser].on();
-
+  int laserIndex = selectedLaser;
+  
   int _x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4;
-  lasers[selectedLaser].getClipArea(_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4);
+  lasers[laserIndex].getClipArea(_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4);
   gpio.setCVTarget(0, _x1, 200);
   gpio.setCVTarget(1, _y1, 200);
   gpio.setCVTarget(2, _x2, 200);
   gpio.setCVTarget(3, _y2, 200);
 
-  gpio.clearCVTarget(4);
-  gpio.clearCVTarget(5);
+  lasers[laserIndex].setColorRGB(255, 0, 255);
+  lasers[laserIndex].on();
 
   while (selectedMode == MODE_CLIPPING_TOP) {
     int x1 = gpio.getCV(0, 0, 4095);
@@ -68,27 +74,34 @@ void clippingTopAlignment() {
     int x2 = gpio.getCV(2, 0, 4095);
     int y2 = gpio.getCV(3, 0, 4095);
    
-    lasers[selectedLaser].setClipAreaTop(x1, y1, x2, y2);
+    lasers[laserIndex].setClipAreaTop(x1, y1, x2, y2);
 
-    lasers[selectedLaser].sendTo(x1 + 10, y1 + 10);
-    lasers[selectedLaser].sendTo(x2 - 10, y2 + 10);
-    lasers[selectedLaser].sendTo(_x3 - 10, _y3 - 10);
-    lasers[selectedLaser].sendTo(_x4 + 10, _y4 - 10);
+    lasers[laserIndex].sendTo(x1 + 10, y1 + 10);
+    lasers[laserIndex].sendTo(x2 - 10, y2 + 10);
+    lasers[laserIndex].sendTo(_x3 - 10, _y3 - 10);
+    lasers[laserIndex].sendTo(_x4 + 10, _y4 - 10);
 
     checkButtons();
+
+    if (selectedLaser != laserIndex)
+      break;
   }
+
+  lasers[laserIndex].off();
 }
 
 void clippingBottomAlignment() {
-  lasers[selectedLaser].setColorRGB(255, 255, 0);
-  lasers[selectedLaser].on();
+  int laserIndex = selectedLaser;
 
   int _x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4;
-  lasers[selectedLaser].getClipArea(_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4);
+  lasers[laserIndex].getClipArea(_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4);
   gpio.setCVTarget(0, _x3, 200);
   gpio.setCVTarget(1, _y3, 200);
   gpio.setCVTarget(2, _x4, 200);
   gpio.setCVTarget(3, _y4, 200);
+
+  lasers[laserIndex].setColorRGB(255, 255, 0);
+  lasers[laserIndex].on();
 
   while (selectedMode == MODE_CLIPPING_BOTTOM) {
     int x3 = gpio.getCV(0, 0, 4095);
@@ -96,27 +109,34 @@ void clippingBottomAlignment() {
     int x4 = gpio.getCV(2, 0, 4095);
     int y4 = gpio.getCV(3, 0, 4095);
    
-    lasers[selectedLaser].setClipAreaBottom(x3, y3, x4, y4);
+    lasers[laserIndex].setClipAreaBottom(x3, y3, x4, y4);
 
-    lasers[selectedLaser].sendTo(_x1 + 10, _y1 + 10);
-    lasers[selectedLaser].sendTo(_x2 - 10, _y2 + 10);
-    lasers[selectedLaser].sendTo(x3 - 10, y3 - 10);
-    lasers[selectedLaser].sendTo(x4 + 10, y4 - 10);
+    lasers[laserIndex].sendTo(_x1 + 10, _y1 + 10);
+    lasers[laserIndex].sendTo(_x2 - 10, _y2 + 10);
+    lasers[laserIndex].sendTo(x3 - 10, y3 - 10);
+    lasers[laserIndex].sendTo(x4 + 10, y4 - 10);
 
     checkButtons();
+
+    if (selectedLaser != laserIndex)
+      break;
   }
+
+  lasers[laserIndex].off();
 }
 
 void warpTopAlignment() {
-  lasers[selectedLaser].setColorRGB(255, 0, 255);
-  lasers[selectedLaser].on();
-
+  int laserIndex = selectedLaser;
+  
   int _x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4;
-  lasers[selectedLaser].getWarpArea(_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4);
+  lasers[laserIndex].getWarpArea(_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4);
   gpio.setCVTarget(0, _x1, 200);
   gpio.setCVTarget(1, _y1, 200);
   gpio.setCVTarget(2, _x2, 200);
   gpio.setCVTarget(3, _y2, 200);
+
+  lasers[laserIndex].setColorRGB(255, 0, 255);
+  lasers[laserIndex].on();
 
   while (selectedMode == MODE_WARP_TOP) {
     int x1 = gpio.getCV(0, 0, 4095);
@@ -124,27 +144,34 @@ void warpTopAlignment() {
     int x2 = gpio.getCV(2, 0, 4095);
     int y2 = gpio.getCV(3, 0, 4095);
    
-    lasers[selectedLaser].setWarpAreaTop(x1, y1, x2, y2);
+    lasers[laserIndex].setWarpAreaTop(x1, y1, x2, y2);
     
-    lasers[selectedLaser].sendTo(x1, y1);
-    lasers[selectedLaser].sendTo(x2, y2);
-    lasers[selectedLaser].sendTo(_x3, _y3);
-    lasers[selectedLaser].sendTo(_x4, _y4);
+    lasers[laserIndex].sendTo(x1, y1);
+    lasers[laserIndex].sendTo(x2, y2);
+    lasers[laserIndex].sendTo(_x3, _y3);
+    lasers[laserIndex].sendTo(_x4, _y4);
 
     checkButtons();
+
+    if (selectedLaser != laserIndex)
+      break;
   }
+
+  lasers[laserIndex].off();
 }
 
 void warpBottomAlignment() {
-  lasers[selectedLaser].setColorRGB(255, 255, 0);
-  lasers[selectedLaser].on();
-
+  int laserIndex = selectedLaser;
+  
   int _x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4;
-  lasers[selectedLaser].getWarpArea(_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4);
+  lasers[laserIndex].getWarpArea(_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4);
   gpio.setCVTarget(0, _x3, 200);
   gpio.setCVTarget(1, _y3, 200);
   gpio.setCVTarget(2, _x4, 200);
   gpio.setCVTarget(3, _y4, 200);
+
+  lasers[laserIndex].setColorRGB(255, 255, 0);
+  lasers[laserIndex].on();
 
   while (selectedMode == MODE_WARP_BOTTOM) {
     int x3 = gpio.getCV(0, 0, 4095);
@@ -152,15 +179,20 @@ void warpBottomAlignment() {
     int x4 = gpio.getCV(2, 0, 4095);
     int y4 = gpio.getCV(3, 0, 4095);
    
-    lasers[selectedLaser].setWarpAreaBottom(x3, y3, x4, y4);
+    lasers[laserIndex].setWarpAreaBottom(x3, y3, x4, y4);
 
-    lasers[selectedLaser].sendTo(_x1, _y1);
-    lasers[selectedLaser].sendTo(_x2, _y2);
-    lasers[selectedLaser].sendTo(x3, y3);
-    lasers[selectedLaser].sendTo(x4, y4);
+    lasers[laserIndex].sendTo(_x1, _y1);
+    lasers[laserIndex].sendTo(_x2, _y2);
+    lasers[laserIndex].sendTo(x3, y3);
+    lasers[laserIndex].sendTo(x4, y4);
 
     checkButtons();
+
+    if (selectedLaser != laserIndex)
+      break;
   }
+
+  lasers[laserIndex].off();
 }
 
 void checkButtons() {
@@ -169,11 +201,12 @@ void checkButtons() {
       selectedMode = MODE_END_CAL;
       return;
     }
-    
+    /*
     if (gpio.isButtonReleased(NES_RIGHT))
       selectedLaser = min(3, selectedLaser + 1);
     if (gpio.isButtonReleased(NES_LEFT))
       selectedLaser = max(0, selectedLaser - 1);
+    */
     /*
     if (gpio.isButtonReleased(NES_UP))
       selectedMode = min(4, selectedMode + 1);
--- software/laser_controller/laser_controller.ino
+++ software/laser_controller/laser_controller.ino
@@ -2,8 +2,7 @@
 #include "Gpio.h"
 #include "Laser.h"
 #include "Drawing.h"
-#include "Objects\Objects.h"
-#include "Objects\Logo.h"
+#include "Objects\Equations.h"
 #include "AudioProcessor.h"
 #include "MeshManager.h"
 #include "Calibration.h"
@@ -20,6 +19,7 @@ ADC *adc = new ADC();
 Gpio gpio;
 AudioProcessor audio;
 DynamicJsonDocument settingsJson(2048);
+int currMode = -1;
 
 void loadSettings() {
   Serial.println("Loading settings");
@@ -124,73 +124,103 @@ void setup() {
         saveSettings();
     } 
   }
+
+  checkMode();
+}
+
+void checkMode() {
+  gpio.readUart();
+  currMode = gpio.getMode();
+  gpio.setLEDs(currMode);
 }
 
 //////////////////
+
+void lasersOff() {
+  for (int i = 0; i < 4; i++)
+    lasers[i].off();
+}
+
+void sendToSpan(int laser, int x, int y) {
+  for (int i = 0; i < 3; i++) {
+    if (i != laser) lasers[laser].off();
+    else lasers[laser].on();
+  }
+  
+  if (x < 4095)
+    lasers[laser].sendTo(x, y);
+  else if (x < 4095 * 2)
+    lasers[laser].sendTo(x - 4095, y);
+  else
+    lasers[laser].sendTo(x - 4095 * 2, y);
+}
 
 #define NUM_BUBBLES 6
 void bubbles() {
   static int xPos[NUM_BUBBLES] = {-1};
   static int yPos[NUM_BUBBLES] = {-1};
   static int rotate[NUM_BUBBLES] = {0};
-  static int color[3] = {255};
+  static int cr[7] = {0,   255, 0,   255, 255, 255, 0};
+  static int cb[7] = {255, 255, 255, 0,   0,   255, 0};
+  static int cg[7] = {255, 0,   0,   0,   255, 255, 255};
+  static int currentColor = 0;
+  static int currentLaser = 0;
 
-  audio.updateFFT();
-  audio.decay = 100;
-  for (int i = 0; i < 3; i++)
-    lasers[i].setDelays(-1, 150);
+  lasersOff();
+  
 
-  bool doReset = true;
-  for (int i = 0; i < NUM_BUBBLES; i++)
-    if (yPos[i] != -1)
-      doReset = false;
-
-  if (doReset) {
-    int x = random(1000, 3000);
-    for (int i = 0; i < NUM_BUBBLES; i++) {
-      xPos[i] = random(x - 500, x + 500);
-      yPos[i] = random(1000, 1400);
-      rotate[i] = random(360);
-      for (int c = 0; c < 3; c++)
-        color[c] = random(255);
-    }
-  }
-
-  for (int i = 0; i < NUM_BUBBLES; i++) {
-    if (yPos[i] == -1)
-      continue;
-
-    for (int i = 0; i < 3; i++)
-      lasers[i].setColorRGB(0, 0, 255);
-      //lasers[i].setColorRGB((int)((millis()) % 255), (int)((millis() + 85) % 255), (int)((millis() + 170) % 255));
-
-    int b = 4;
-    float firstX = 2048;
-    float firstY = 2048;
-    for (int r = 0; r <= 360; r+=45, b++) {    
-      float d = ((float)audio.bands[b] / 25 + 5) * map(yPos[i], 0, 4000, 20, 70) / 6;
-      float x = SIN((r + rotate[i]) % 360) * d + xPos[i] + SIN(yPos[i]) * 50 + SIN(xPos[i] * 50);
-      float y = COS((r + rotate[i]) % 360) * d + yPos[i];
-      for (int i = 0; i < 3; i++)
-        lasers[i].sendTo(x, y);
-      if (r == 0) {    
-        for (int i = 0; i < 3; i++)
-          lasers[i].on();
-        firstX = x;
-        firstY = y;
+  while (currMode == 2) {
+    audio.updateFFT();
+    audio.decay = 100;
+   
+    bool doReset = true;
+    for (int i = 0; i < NUM_BUBBLES; i++)
+      if (yPos[i] != -1)
+        doReset = false;
+  
+    if (doReset) {
+      int x = random(1000, 3000);
+      //currentLaser = (currentLaser + 1) % 3;
+      currentColor = (currentColor + 1) % 7;
+      lasers[currentLaser].setColorRGB(cr[currentColor], cb[currentColor], cg[currentColor]);
+      lasers[currentLaser].setDelays(-1, 150);
+      for (int i = 0; i < NUM_BUBBLES; i++) {
+        xPos[i] = random(x - 500, x + 500);
+        yPos[i] = random(1000, 1400);
+        rotate[i] = random(360);
       }
     }
+  
+    for (int i = 0; i < NUM_BUBBLES; i++) {
+      if (yPos[i] == -1)
+        continue;
+  
+      int b = 4;
+      float firstX = 2048;
+      float firstY = 2048;
+      for (int r = 0; r <= 360; r+=45, b++) {    
+        float d = ((float)audio.bands[b] / 25 + 5) * map(yPos[i], 0, 4000, 20, 70) / 6;
+        float x = SIN((r + rotate[i]) % 360) * d + xPos[i] + SIN(yPos[i]) * 50 + SIN(xPos[i] * 50);
+        float y = COS((r + rotate[i]) % 360) * d + yPos[i];
+        lasers[currentLaser].sendTo(x, y);
+        if (r == 0) { 
+          lasers[currentLaser].on();
+          firstX = x;
+          firstY = y;
+        }
+      }
+  
+      yPos[i] += random(5, 15);
+      if (yPos[i] > 3200)
+        yPos[i] = -1;
+      rotate[i] += 3;
+  
+      lasers[currentLaser].sendTo(firstX, firstY);
+      lasers[currentLaser].off();
+    }  
 
-    yPos[i] += random(5, 15);
-    if (yPos[i] > 3200)
-      yPos[i] = -1;
-    rotate[i] += 3;
-
-    for (int i = 0; i < 3; i++) {
-      lasers[i].sendTo(firstX, firstY);
-      lasers[i].off();
-    }
-  }  
+    checkMode();
+  }
 }
 
 
@@ -200,41 +230,44 @@ void quads() {
   static int xDir[] = {1, 1, -1, -1};
   static int yDir[] = {1, -1, -1, 1};
 
-  //audio.updateFFT();
-  //audio.decay = 100;
+  lasersOff();
+  lasers[0].setColorRGB(255, 0, 255);
+  lasers[0].on();
+  lasers[0].setDelays(-1, 350);
 
-  for (int i = 0; i < 3; i++) {
-    lasers[i].setColorRGB(0, 255, 0);
-    //lasers[i].setColorRGB((int)((millis()) % 255), (int)((millis() + 85) % 255), (int)((millis() + 170) % 255));
-    lasers[i].on();
-    lasers[i].setDelays(-1, 350);
-
+  while (currMode == 3) {
     for (int p = 0; p < 4; p++)
-      lasers[i].sendTo(xPos[p], yPos[p]);
-      //lasers[i].sendTo(xPos[p] + audio.bands[1], yPos[p] + audio.bands[1]);
-  }
+      lasers[0].sendTo(xPos[p], yPos[p]);
+      
+    for (int p = 0; p < 4; p++) {
+      xPos[p] += xDir[p] * random(10);
+      if (xPos[p] > 2500 || xPos[p] < 1500) xDir[p] *= -1;
+      yPos[p] += yDir[p] * random(10);
+      if (yPos[p] > 2500 || yPos[p] < 1500) yDir[p] *= -1;
+    }
 
-  for (int p = 0; p < 4; p++) {
-    xPos[p] += xDir[p] * random(30);
-    if (xPos[p] > 2500 || xPos[p] < 1500) xDir[p] *= -1;
-    yPos[p] += yDir[p] * random(30);
-    if (yPos[p] > 2500 || yPos[p] < 1500) yDir[p] *= -1;
+    checkMode();
   }
 }
 
 void lissajou() {
-  for (int i = 0; i < 3; i++) {
-    lasers[i].setColorRGB(0, 0, 255);
-    //lasers[i].setColorRGB((int)((millis() / 300) % 255), (int)((millis() / 300 + 85) % 255), (int)((millis() / 300 + 170) % 255));
-    lasers[i].on();
-    lasers[i].setDelays(-1, 150);
-  }
+  static int masterX = 0;
 
-  for (int i = 0; i < 720; i+=10) {
-    int x = (int)(sin((millis() / 30 + i) * PI / 360) * 500 + 2048);
-    int y = (int)(cos((millis() / 40 - i * 2) * PI / 180) * 500 + 2048);
-    for (int i = 0; i < 3; i++)
-      lasers[i].sendTo(x, y);
+  lasersOff();
+  lasers[0].setDelays(-1, 150);
+  lasers[0].setColorRGB(255, 0, 255);
+  lasers[0].on();
+
+  while (currMode == 6) {
+    for (int i = 0; i < 720; i+=10) {
+      int x = (int)(sin((millis() / 30 + i) * PI / 360) * 500 + 2048);
+      int y = (int)(cos((millis() / 40 - i * 2) * PI / 180) * 500 + 2048);
+      lasers[0].sendTo(x, y);
+    }
+    
+    masterX += 15;
+
+    checkMode();
   }
 }
 
@@ -247,58 +280,136 @@ void etchasketch() {
   static int currY = 2048;
   static bool currPen = true;
 
-  for (int i = 0; i < 3; i++) {
-    lasers[i].setColorRGB(0, 255, 0);
-    lasers[i].setDelays(-1, 350);
-    lasers[i].off();
+  lasersOff();
+  lasers[0].setDelays(-1, 350);
+
+  while (currMode == 7) {
+    gpio.readUart();
+  
+    if (gpio.isButtonReleased(NES_SELECT))
+      numPoints = 0;
+  
+    if (gpio.isButtonReleased(NES_B))
+      currPen = !currPen;
+  
+    if (gpio.isButtonReleased(NES_A)) {
+      xPos[numPoints] = currX;
+      yPos[numPoints] = currY;
+      penDown[numPoints] = currPen;
+      numPoints++;
+    }
+  
+    if (gpio.isButtonPressed(NES_LEFT))  currX = constrain(currX - 8, 1500, 2500);
+    if (gpio.isButtonPressed(NES_RIGHT)) currX = constrain(currX + 8, 1500, 2500);
+    if (gpio.isButtonPressed(NES_UP))    currY = constrain(currY + 8, 1500, 2500);
+    if (gpio.isButtonPressed(NES_DOWN))  currY = constrain(currY - 8, 1500, 2500);
+  
+    lasers[0].setColorRGB(0, 255, 0);
+    for (int i = 0; i < numPoints; i++) {
+      if (penDown[i] && i > 0) lasers[0].on();
+      else lasers[0].off();
+      lasers[0].sendTo(xPos[i], yPos[i]);
+    }
+  
+    if (currPen) lasers[0].setColorRGB(0, 0, 255);
+    else lasers[0].setColorRGB(255, 0, 0);
+  
+    lasers[0].on();
+    lasers[0].sendTo(currX, currY);
+    delay(50);
+    lasers[0].off();
+
+    checkMode();
   }
+}
 
-  gpio.readUart();
-
-  if (gpio.isButtonReleased(NES_SELECT))
-    numPoints = 0;
-
-  if (gpio.isButtonReleased(NES_B))
-    currPen = !currPen;
-
-  if (gpio.isButtonReleased(NES_A)) {
-    xPos[numPoints] = currX;
-    yPos[numPoints] = currY;
-    penDown[numPoints] = currPen;
-    numPoints++;
+#define NUM_EQUATIONS 39
+void _drawEq(int i, int x, int y) {
+  switch (i) {
+    case 0: Drawing::drawObject(draw_quadratic, sizeof(draw_quadratic) / 4, x, y, 1); break;
+    case 1: Drawing::drawObject(draw_eqn01b, sizeof(draw_eqn01b) / 4, x, y, 1); break;
+    case 2: Drawing::drawObject(draw_eqn02, sizeof(draw_eqn02) / 4, x, y, 1); break;
+    case 3: Drawing::drawObject(draw_eqn02a, sizeof(draw_eqn02a) / 4, x, y, 1); break;
+    case 4: Drawing::drawObject(draw_eqn02b, sizeof(draw_eqn02b) / 4, x, y, 1); break;
+    case 5: Drawing::drawObject(draw_eqn03, sizeof(draw_eqn03) / 4, x, y, 1); break;
+    case 6: Drawing::drawObject(draw_eqn04, sizeof(draw_eqn04) / 4, x, y, 1); break;
+    case 7: Drawing::drawObject(draw_eqn05, sizeof(draw_eqn05) / 4, x, y, 1); break;
+    case 8: Drawing::drawObject(draw_eqn06, sizeof(draw_eqn06) / 4, x, y, 1); break;
+    case 9: Drawing::drawObject(draw_eqn07, sizeof(draw_eqn07) / 4, x, y, 1); break;
+    case 10: Drawing::drawObject(draw_eqn08, sizeof(draw_eqn08) / 4, x, y, 1); break;
+    case 11: Drawing::drawObject(draw_eqn09, sizeof(draw_eqn09) / 4, x, y, 1); break;
+    case 12: Drawing::drawObject(draw_eqn10, sizeof(draw_eqn10) / 4, x, y, 1); break;
+    case 13: Drawing::drawObject(draw_eqn11, sizeof(draw_eqn11) / 4, x, y, 1); break;
+    case 14: Drawing::drawObject(draw_eqn12, sizeof(draw_eqn12) / 4, x, y, 1); break;
+    case 15: Drawing::drawObject(draw_eqn13, sizeof(draw_eqn13) / 4, x, y, 1); break;
+    case 16: Drawing::drawObject(draw_eqn14, sizeof(draw_eqn14) / 4, x, y, 1); break;
+    case 17: Drawing::drawObject(draw_eqn14, sizeof(draw_eqn14) / 4, x, y, 1); break;
+    case 18: Drawing::drawObject(draw_eqn15, sizeof(draw_eqn15) / 4, x, y, 1); break;
+    case 19: Drawing::drawObject(draw_eqn16, sizeof(draw_eqn16) / 4, x, y, 1); break;
+    case 20: Drawing::drawObject(draw_eqn17, sizeof(draw_eqn17) / 4, x, y, 1); break;
+    case 21: Drawing::drawObject(draw_eqn18, sizeof(draw_eqn18) / 4, x, y, 1); break;
+    case 22: Drawing::drawObject(draw_eqn19, sizeof(draw_eqn19) / 4, x, y, 1); break;
+    case 23: Drawing::drawObject(draw_eqn20, sizeof(draw_eqn20) / 4, x, y, 1); break;
+    case 24: Drawing::drawObject(draw_eqn21, sizeof(draw_eqn21) / 4, x, y, 1); break;
+    case 25: Drawing::drawObject(draw_eqn22, sizeof(draw_eqn22) / 4, x, y, 1); break;
+    case 26: Drawing::drawObject(draw_eqn23, sizeof(draw_eqn23) / 4, x, y, 1); break;
+    case 27: Drawing::drawObject(draw_eqn24, sizeof(draw_eqn24) / 4, x, y, 1); break;
+    case 28: Drawing::drawObject(draw_eqn25, sizeof(draw_eqn25) / 4, x, y, 1); break;
+    case 29: Drawing::drawObject(draw_eqn26, sizeof(draw_eqn26) / 4, x, y, 1); break;
+    case 30: Drawing::drawObject(draw_eqn27, sizeof(draw_eqn27) / 4, x, y, 1); break;
+    case 31: Drawing::drawObject(draw_eqn28, sizeof(draw_eqn28) / 4, x, y, 1); break;
+    case 32: Drawing::drawObject(draw_eqn29, sizeof(draw_eqn29) / 4, x, y, 1); break;
+    case 33: Drawing::drawObject(draw_eqn30, sizeof(draw_eqn30) / 4, x, y, 1); break;
+    case 34: Drawing::drawObject(draw_eqn31, sizeof(draw_eqn31) / 4, x, y, 1); break;
+    case 35: Drawing::drawObject(draw_eqn32, sizeof(draw_eqn32) / 4, x, y, 1); break;
+    case 36: Drawing::drawObject(draw_eqn33, sizeof(draw_eqn33) / 4, x, y, 1); break;
+    case 37: Drawing::drawObject(draw_eqn35, sizeof(draw_eqn35) / 4, x, y, 1); break;
+    case 38: Drawing::drawObject(draw_eqn36, sizeof(draw_eqn36) / 4, x, y, 1); break;
   }
-
-  if (gpio.isButtonPressed(NES_LEFT))  currX = constrain(currX - 8, 1500, 2500);
-  if (gpio.isButtonPressed(NES_RIGHT)) currX = constrain(currX + 8, 1500, 2500);
-  if (gpio.isButtonPressed(NES_UP))    currY = constrain(currY + 8, 1500, 2500);
-  if (gpio.isButtonPressed(NES_DOWN))  currY = constrain(currY - 8, 1500, 2500);
-
-
-  for (int i = 0; i < numPoints; i++) {
-    if (penDown[i] && i > 0) lasers[1].on();
-    else lasers[1].off();
-    lasers[1].sendTo(xPos[i], yPos[i]);
-  }
-
-  if (currPen) lasers[1].on();
-  else lasers[1].off();
-  lasers[1].sendTo(currX, currY);
-  lasers[1].on();
-  delay(50);
-  lasers[1].off();
 }
 
 void equations() {
-  
+  static int xPos = 1500;
+  static int yPos = 1500;
+  static int xDir = 1;
+  static int yDir = 1;
+  static int currEquation = 0;
+  static unsigned long lastEquationUpdate = millis();
+
+  lasersOff();
+  lasers[0].setColorRGB(0, 255, 0);
+  lasers[0].on();
+  lasers[0].setDelays(-1, 350);
+
+  while (currMode == 4) {
+    _drawEq(currEquation, xPos, yPos);
+      
+    xPos += xDir * 14;
+    if (xPos > 2500 || xPos < 1500)
+      xDir *= -1;
+    
+    yPos += yDir * 8;
+    if (yPos > 2500 || yPos < 1500)
+      yDir *= -1;
+
+    unsigned long newEquationTime = millis();
+    if (newEquationTime - lastEquationUpdate > 10000) {
+      lastEquationUpdate = newEquationTime;
+      currEquation = (currEquation + 1) % NUM_EQUATIONS;
+    }
+
+    checkMode();
+  }
 }
 
 void graphics() {
-  
-}
+  //setup
+  lasersOff();
 
-void lasersOff() {
-  for (int i = 0; i < 4; i++) {
-    lasers[i].off();
+  while (currMode == 5) {
+    //loop
+
+    checkMode();
   }
 }
 
@@ -306,9 +417,8 @@ void lasersOff() {
 
 
 void loop() { 
-  gpio.readUart();
-  gpio.setLEDs(gpio.getMode());
-  switch (gpio.getMode()) {
+  checkMode();
+  switch (currMode) {
     case 2: 
       bubbles();
       break;
