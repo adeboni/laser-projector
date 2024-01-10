91c234e81b3dfec310cfdeb98f6cc0ab517262bd
Alex DeBoni <8951697+adeboni@users.noreply.github.com>
2024-01-10 13:49:45 -0800

    Switched ui from tkinter to pygame

--- 2024/software/server/app.py
+++ 2024/software/server/app.py
@@ -1,15 +1,19 @@
 """Main entry point"""
-import tkinter as tk
+import pygame 
 from laser_server import LaserServer
 from song_handler import SongHandler
 from sacn_handler import SACNHandler
-
-class MainApp(tk.Tk):
+  
+class MainApp:
     """Class representing the GUI"""
-    def __init__(self, num_lasers: int, host_ip: str, target_ip: str, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.title('Laser Control Station')
-        self.geometry('1000x300')
+    def __init__(self, num_lasers: int, host_ip: str, target_ip: str) -> None:
+        pygame.init()
+        self.window_size = (750, 300)
+        self.font = pygame.font.SysFont('Arial', 32)
+        self.labels = {'Mode': '0',
+                       'Song Input': 'A0',
+                       'Current Song': 'None',
+                       'Song Queue': 'Empty'}
 
         self.songs = SongHandler()
         self.current_letter = ord('A')
@@ -22,77 +26,79 @@ class MainApp(tk.Tk):
         self.sacn = SACNHandler(target_ip)
         self.sacn.start()
 
-        self._setup_labels()
-        self._setup_binding()
-        self._update_song_status()
+    def show_screen(self) -> None:
+        screen = pygame.display.set_mode(self.window_size)
+        pygame.display.set_caption('Laser Control Station')
+        clock = pygame.time.Clock()
+        time_since_song_update = 0
 
-        self._update_selection(None)
-        self._update_mode(None)
+        def update_display():
+            screen.fill((255, 255, 255))
+            self._draw_labels(screen)
+            pygame.display.update()
 
+        while True:
+            time_since_song_update += clock.tick()
+            if time_since_song_update > 100:
+                time_since_song_update = 0
+                self._update_song_status()
+                update_display()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.sacn.stop()
+                    pygame.quit()
+                    quit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key in range(48, 58):
+                        self.current_mode = event.key - 48
+                        self.laser_server.mode = self.current_mode
+                        self.labels['Mode'] = f'{self.current_mode}'
+                    elif event.key in [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]:
+                        self._update_selection(event.key)
+                    elif event.key == pygame.K_RETURN:
+                        self.songs.add_to_queue(self.current_letter, self.current_number)
+                    elif event.key == pygame.K_SPACE:
+                        self.songs.play_next_song()
+
+                update_display()
+                
     def start_server(self) -> None:
         self.laser_server.start_server()
 
-    def _on_closing(self) -> None:
-        self.sacn.stop()
-        self.destroy()
+    def _draw_labels(self, screen: pygame.Surface) -> None:
+        for i, (name, value) in enumerate(self.labels.items()):
+            text = self.font.render(f'{name}: {value}', True, (0, 0, 0))
+            rect = text.get_rect()
+            rect.center = (self.window_size[0] // 2, 15 + i * 40)
+            screen.blit(text, rect)
 
-    def _setup_labels(self) -> None:
-        self.mode = tk.Label(self, font=('Arial', 25))
-        self.mode.pack()
-
-        self.song_selection = tk.Label(self, font=('Arial', 25))
-        self.song_selection.pack()
-
-        self.current_song = tk.Label(self, font=('Arial', 25))
-        self.current_song.pack()
-
-        self.song_queue = tk.Label(self, font=('Arial', 25))
-        self.song_queue.pack()
-
-    def _setup_binding(self) -> None:
-        for c in ['<Up>', '<Down>', '<Left>', '<Right>']:
-            self.bind(c, self._update_selection)
-        for i in range(8):
-            self.bind(str(i), self._update_mode)
-        self.bind('<Return>', lambda _e: self.songs.add_to_queue(self.current_letter, self.current_number))
-        self.bind('<space>', lambda _e: self.songs.play_next_song())
-        self.protocol("WM_DELETE_WINDOW", self._on_closing)
-
-    def _update_selection(self, e) -> None:
-        if e is not None:
-            if e.keycode == 38 and chr(self.current_letter) != self.songs.get_booklet_letter_limit():
-                self.current_letter += 1
-            elif e.keycode == 40 and chr(self.current_letter) != 'A':
-                self.current_letter -= 1
-            elif e.keycode == 37 and self.current_number != 0:
-                self.current_number -= 1
-            elif e.keycode == 39 and self.current_number != 9:
-                self.current_number += 1
-        self.song_selection.config(text=f'Song Input: {chr(self.current_letter)}{self.current_number}')
-
-    def _update_mode(self, e) -> None:
-        if e is not None:
-            self.current_mode = int(e.char)
-            self.laser_server.mode = self.current_mode
-        self.mode.config(text=f'Mode: {self.current_mode}')
+    def _update_selection(self, key: int) -> None:
+        if key == pygame.K_UP and chr(self.current_letter) != self.songs.get_booklet_letter_limit():
+            self.current_letter += 1
+        elif key == pygame.K_DOWN and chr(self.current_letter) != 'A':
+            self.current_letter -= 1
+        elif key == pygame.K_LEFT and self.current_number != 0:
+            self.current_number -= 1
+        elif key == pygame.K_RIGHT and self.current_number != 9:
+            self.current_number += 1
+        self.labels['Song Input'] = f'{chr(self.current_letter)}{self.current_number}'
 
     def _update_song_status(self) -> None:
         self.songs.update()
 
         if self.songs.current_song is None:
-            self.current_song.config(text='Current Song: None')
+            self.labels['Current Song'] = 'None'
         else:
-            self.current_song.config(text=f'Current Song: {self.songs.current_song.running_str}')
+            self.labels['Current Song'] = self.songs.current_song.running_str
 
         if any(self.songs.song_queue):
-            self.song_queue.config(text=f'Song Queue: {", ".join([x.song_id_str for x in self.songs.song_queue])}')
+            self.labels['Song Queue'] = ", ".join([x.song_id_str for x in self.songs.song_queue])
         else:
-            self.song_queue.config(text='Song Queue: Empty')
-
-        self.after(100, self._update_song_status)
+            self.labels['Song Queue'] = 'Empty'
                 
 if __name__ == '__main__':
     app = MainApp(num_lasers=3, host_ip='127.0.0.1', target_ip='127.0.0.1')
     # app = MainApp(num_lasers=3, host_ip='10.0.0.2', target_ip='10.0.0.20')
     app.start_server()
-    app.mainloop()
+    app.show_screen()
