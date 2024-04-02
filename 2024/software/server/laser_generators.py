Fixed wand rotation calculation

2024/software/server/laser_generators.py
@@ -167,7 +167,7 @@ def audio_visualization(num_lasers: int) -> Generator[list[LaserPoint], None, No
     sample_interval = 8
     xs = np.linspace(_xs[1], _xs[2], num=sample_blocksize)
     ys = [base_y for _ in range(sample_blocksize)]
-    rgb = [255, 0, 0]
+    rgb = [0, 255, 0]
     index = 0
 
     while True:

2024/software/server/sierpinski.py
@@ -168,17 +168,20 @@ def joystick_quaternion():
         yield wand_offset.rotate(q)
         
 def joystick_sim():
-    wand_offset = pyquaternion.Quaternion(1, 0, 0, -1)
     q_init = pyquaternion.Quaternion(w=0.280, x=0.284, y=0.882, z=0.252)
 
     start = np.array([1, 0, 0])
     target_vector = np.array([center_point[0], center_point[1], center_point[2] - HUMAN_HEIGHT])
     q_target = find_quat(start, target_vector)
-    q_div = q_target / q_init
+    
+    v1 = q_target.rotate(start)
+    x = np.array([-1.0 * v1[0] * v1[2], -1.0 * v1[1] * v1[2], v1[0]**2 + v1[1]**2]) / np.sqrt(v1[0]**2 + v1[1]**2) 
+    
+    print(v1, v2)
     
     while True:
         q = q_init
-        yield q_div * q
+        yield (q_target / q_init) * q
 
 def mouse_quaternion():
     import pyautogui
@@ -192,7 +195,7 @@ def mouse_quaternion():
         yield mouse_offset * find_quat(start, end)
 
 if __name__ == '__main__':
-    quaternion_generator = joystick_quaternion()
+    quaternion_generator = joystick_sim()
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')

2024/software/server/wand.py
@@ -26,15 +26,15 @@ class Wand:
     def quit(self) -> None:
         self.joystick.quit()
         
-    # todo: make this get the angle around the [1, 0, 0] axis
     def get_rotation_angle(self) -> int:
-        x, y, z = self.position.rotate([0, 1, 0])
-        x, y, z = x / np.sqrt(x**2 + y**2 + z**2), y / np.sqrt(x**2 + y**2 + z**2), z / np.sqrt(x**2 + y**2 + z**2)
-        phi = np.arctan2(y, x)
-        return int(np.degrees(phi)) % 360
-
+        v0 = self.position.rotate([0, 1, 0])
+        v1 = self.position.rotate([1, 0, 0])
+        v2 = np.array([-1.0 * v1[0] * v1[2], -1.0 * v1[1] * v1[2], v1[0]**2 + v1[1]**2]) / np.sqrt(v1[0]**2 + v1[1]**2)
+        phi = np.arccos(np.dot(v0, v2))
+        return int(np.degrees(phi)) % 180
+    
     def get_wand_color(self) -> list[int]:
-        r, g, b = colorsys.hsv_to_rgb(self.get_rotation_angle() / 360, 1, 1)
+        r, g, b = colorsys.hsv_to_rgb(self.get_rotation_angle() / 180, 1, 1)
         return [int(r * 255), int(g * 255), int(b * 255)]
 
     def get_laser_point(self) -> laser_point.LaserPoint:
@@ -71,3 +71,4 @@ class Wand:
             if self.callback:
                 self.callback()
         self.prev_speed = new_speed
+        