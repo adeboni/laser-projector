bfd7308e8bebf1c4e0d68ed018923fb9a973fbb3
Alex DeBoni <8951697+adeboni@users.noreply.github.com>
2024-04-12 14:22:08 -0700

    Update synthesizer.py

--- 2024/software/server/synthesizer.py
+++ 2024/software/server/synthesizer.py
@@ -19,21 +19,49 @@ class SynthServer:
 
     def __init__(self, ) -> None:
         self.server = None
-        self.synth = None
+        self.synths = {}
 
-    def start(self, synth_def) -> None:
+    def start_server(self) -> None:
         self.server = supriya.Server().boot()
-        self.server.add_synthdefs(synth_def)
+        self.server.add_synthdefs(simple_sine)
+        self.server.add_synthdefs(sci_fi)
         self.server.sync()
-        self.synth = self.server.add_synth(synth_def)
-
-    def stop(self) -> None:
-        if self.synth:
-            self.synth.free()
-            self.synth = None
+        
+    def stop_server(self) -> None:
+        for synth in list(self.synths):
+            self.stop_synth(synth)
         if self.server:
-            self.server.quit()
-            self.server = None
+            try:
+                self.server.quit()
+            except TimeoutError:
+                pass
 
-    def update_synth(self, **kwargs) -> None:
-        self.synth.set(**kwargs)
+    def start_synth(self, id: int, synth_def: supriya.synthdefs.synthdefs.SynthDef) -> None:
+        self.synths[id] = self.server.add_synth(synth_def)
+
+    def stop_synth(self, id: int) -> None:
+        self.synths[id].free()
+        del self.synths[id]
+
+    def update_synth(self, id: int, **kwargs) -> None:
+        self.synths[id].set(**kwargs)
+
+if __name__ == '__main__':
+    import pyautogui
+    import numpy as np
+
+    screen_width, screen_height = pyautogui.size()
+    server = SynthServer()
+    server.start_server()
+    id = 0
+    server.start_synth(id, sci_fi)
+
+    try:
+        while True:
+            mouse = pyautogui.position()
+            freq = np.interp(mouse.y, [0, screen_width], [1200, 300])
+            x = np.interp(mouse.x, [0, screen_height], [3, 15])
+            server.update_synth(id, freq=freq, x=x)
+    except KeyboardInterrupt:
+        server.stop_server()
+    
