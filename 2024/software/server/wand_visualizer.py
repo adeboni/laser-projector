8062a19683912384bc938773c404379f7798e60c
Alex DeBoni <8951697+adeboni@users.noreply.github.com>
2024-04-25 14:56:17 -0700

    Update wand_visualizer.py

--- 2024/software/server/wand_visualizer.py
+++ 2024/software/server/wand_visualizer.py
@@ -45,7 +45,7 @@ def quaternion_between_vectors(v1, v2):
 
 q_init = quaternion_between_vectors(endpoints[1], endpoints[0])
 
-def euler_angles_between_vectors(v1, v2):
+def euler_angles(v1, v2):
     dot = np.dot(v1, v2)
     cross = np.cross(v1, v2)
     yaw = np.arctan2(cross[1], cross[0])
@@ -53,8 +53,11 @@ def euler_angles_between_vectors(v1, v2):
     roll = np.arctan2(dot, np.sqrt(1 - dot**2))
     return (np.degrees(yaw), np.degrees(pitch), np.degrees(roll))
 
-next_print = 0
+def two_angles(v1, v2):
+    v = v2 - v1
+    return (np.degrees(-np.arcsin(v[1])), np.degrees(np.arcsin(v[2])))
 
+next_print = 0
 def animate(_):
     global next_print
     wands[0].update_position()
@@ -64,7 +67,7 @@ def animate(_):
         line.set_data([0, new_end[0]], [0, new_end[1]])
         line.set_3d_properties([0, new_end[2]])
         if time.time() > next_print and i == 1:
-            print(f'{end} -> {new_end} = {euler_angles_between_vectors(end, new_end)}')
+            print(f'{end} -> {new_end} = {two_angles(end, new_end)}')
             next_print = time.time() + 0.25
 
 ani = animation.FuncAnimation(fig, animate, interval=25, cache_frame_data=False)
